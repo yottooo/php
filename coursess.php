5557da89ea9d97d748d917630797a5bf51c14e42
Yo <ednok2@abv.bg>
2015-03-02 01:13:00 +0200

    tablciata na courses

--- coursess.php
+++ coursess.php
@@ -12,17 +12,28 @@ and open the template in the editor.
     </head>
     <body>
     <a href="courseadd.html">Нов курс</a><br>
-    <form action="">
+    <form method="GET">
        Име на Курс:<input type="text" name="kurs-ime" ><button type="submit">Търси</button>
     </form><br>
     <table width="200" border="1">
       <tr>
-        <th scope="col">#</th>
-        <th scope="col">Име</th>
-        <th scope="col">&nbsp;</th>
-        <th scope="col">Операции</th>
+        <th >#</th>
+        <th >Име</th>
+        <th >Операции</th>
       </tr>
-    </table>
-    
+    <?php
+    $mysqli = new mysqli('localhost', 'root', '', 'students'); 
+$mysqli->set_charset('utf8'); 
+if($_GET["kurs-ime"]!=""){
+$result = $mysqli->query("SELECT * FROM courses where course_name='". $_GET["kurs-ime"] ."'");
+} else {$result = $mysqli->query("SELECT * FROM courses ");}
+$mysqli->close();
+while ($row=$result->fetch_assoc())
+{
+    echo "<tr><th>".$row["course_id"]."</th>"."<th>".$row["course_name"]."</th><th><a >DEL</a>"." "."<a>Редакция </a></th>";
+}
+            ;
+    ?>
+          </table>
     </body>
 </html>
