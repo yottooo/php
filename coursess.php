12190c4b2f907bebd7583dd3449890653cd5f5a8
Yo <ednok2@abv.bg>
2015-03-02 01:25:40 +0200

    css

--- coursess.php
+++ coursess.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
+<?php include 'before.html';?>
     <a href="courseadd.html">Нов курс</a><br>
     <form method="GET">
        Име на Курс:<input type="text" name="kurs-ime" ><button type="submit">Търси</button>
