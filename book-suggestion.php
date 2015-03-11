e018c1f9bcce09d6ffe0f5f8258fdfbbd68e1df7
Yo <ednok2@abv.bg>
2015-03-11 21:02:47 +0200

    vij ajaxa ! :D

---
+++ book-suggestion.php
@@ -0,0 +1,12 @@
+<?php  
+$mysqli = new mysqli('localhost', 'root', '', 'students'); 
+$mysqli->set_charset('utf8');
+
+$book_name = $_POST['book_name'];  
+$sql = "select student_fname from students where student_fname LIKE '$book_name%'";  
+$result = $mysqli->query($sql);  
+while($row=  mysqli_fetch_array($result))  
+{  
+echo "<p>".$row['student_fname']."</p>";  
+}  
+?> 
